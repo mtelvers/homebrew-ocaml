298c13a3f97f0e009d368918959ae9af4e9ac994
Anil Madhavapeddy <anil@recoil.org>
2023-06-18 18:11:56 +0100

    add a test OCaml developer tools formula

---
+++ Formula/ocaml-dev-tools.rb
@@ -0,0 +1,25 @@
+class Ocaml_dev_tools < Formula
+  desc "The OCaml compiler and accompanying developer tools"
+  homepage "https://ocaml.org/"
+  license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
+  head "https://github.com/ocaml/ocaml.git", branch: "trunk"
+
+  stable do
+    url "https://caml.inria.fr/pub/distrib/ocaml-5.0/ocaml-5.0.0.tar.xz"
+    sha256 "cb17f0a534dd4b3fec93d011b0be1ba5c6087da033cf789e85a34b9641ecc65f"
+  end
+
+  pour_bottle? only_if: :default_prefix
+
+  def install
+    args = %W[
+      --prefix=#{HOMEBREW_PREFIX}/ocaml/5.0/dev-tools
+      --enable-debug-runtime
+      --mandir=#{man}
+    ]
+    system "./configure", *args
+    system "make", "world.opt"
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+end
