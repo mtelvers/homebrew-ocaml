8921b53c8ee3810e6eb0669033edd25e36eb303b
Anil Madhavapeddy <anil@recoil.org>
2023-06-20 09:22:57 +0000

    ocaml-dev-tools: add 5.0.0 bottle.

--- Formula/ocaml-dev-tools.rb
+++ Formula/ocaml-dev-tools.rb
@@ -9,6 +9,12 @@ class OcamlDevTools < Formula
     sha256 "cb17f0a534dd4b3fec93d011b0be1ba5c6087da033cf789e85a34b9641ecc65f"
   end
 
+  bottle do
+    root_url "https://github.com/avsm/homebrew-ocaml/releases/download/ocaml-dev-tools-5.0.0"
+    sha256 cellar: :any,                 ventura:      "558fd83d4506233a9de826d2daae332c19d5549d38aecdda93447c6abf3a3854"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "36804b147172abd35081122a7aa54df1ed0fac89ffe828a483bbd9ed5dc7457b"
+  end
+
   pour_bottle? only_if: :default_prefix
 
   def install
