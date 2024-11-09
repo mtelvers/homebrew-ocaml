5008f58942ff890ec0f34b2f0277794adc4c7995
Anil Madhavapeddy <anil@recoil.org>
2024-11-09 17:04:33 +0000

    more

--- bin/oxcaml.sh
+++ bin/oxcaml.sh
@@ -2,12 +2,12 @@
 
 ROOT=${1:-%%PREFIX%%}
 
-export OPAMROOT=$ROOT/.opam
+export OPAMROOT="${ROOT}/.opam"
 export OPAMYES=1
 
-if [ ! -d "$ROOT" ]; then
+if [[ ! -d "${ROOT}" ]]; then
   echo Initialising opam for OxCaml
-  mkdir -p "$ROOT"
+  mkdir -p "${ROOT}"
   opam init -n --bare
   opam switch create oxcaml --empty
   opam repo add with-extensions "https://github.com/janestreet/opam-repository.git#with-extensions"
