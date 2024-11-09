88f519ae04a1a0ad66cc65c67d1e9701ff4c4df4
Anil Madhavapeddy <anil@recoil.org>
2024-11-09 15:48:55 +0000

    some helper scripts

---
+++ bin/oxcaml.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+ROOT=${1:-%%PREFIX%%}
+
+export OPAMROOT=$ROOT/.opam
+export OPAMYES=1
+
+if [ ! -d "$ROOT" ]; then
+  echo Initialising opam for OxCaml
+  mkdir -p "$ROOT"
+  opam init -n --bare
+  opam switch create oxcaml --empty
+  opam repo add with-extensions "https://github.com/janestreet/opam-repository.git#with-extensions"
+  opam install -y ocaml-variants.5.2.0+flambda2 ocaml-lsp-server
+fi
