f02bdcd762098e38a23354b429fe2c530203aca6
Mark Elvers <mark.elvers@tunbury.org>
2025-10-07 11:23:08 +0000

    Add day10-tui
    Closes #2.
    
    Signed-off-by: Mark Elvers <10965410+mtelvers@users.noreply.github.com>

---
+++ Formula/day10tui.rb
@@ -0,0 +1,18 @@
+class Day10tui < Formula
+  desc "Webp image generator for responsive HTML sites"
+  homepage "https://github.com/mtelvers/day10-tui.git/"
+  url "https://github.com/mtelvers/day10-tui.git", branch: "master"
+  version "0.0.1"
+  license "ISC"
+
+  depends_on "apache-arrow"
+  depends_on "gpatch"
+  depends_on "libffi"
+  depends_on "opam"
+  depends_on "pkg-config"
+
+  def install
+    system "bash", "./.opambuild.sh"
+    bin.install "_opam/bin/day10-tui"
+  end
+end
